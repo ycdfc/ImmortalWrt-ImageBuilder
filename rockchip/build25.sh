e01ed1f68090079f1470e43ffff8db7862d5ea38
ycdfc <71202748+ycdfc@users.noreply.github.com>
2026-07-15 12:25:23 +0800

    Update build25.sh

--- rockchip/build25.sh
+++ rockchip/build25.sh
@@ -55,6 +55,7 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-i18n-adguardhome-zh-cn"
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
